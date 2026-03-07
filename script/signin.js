ed036c886be33b9b53777d8ac704ecf8c5458588
mehedi hasan <mehedihasan.j135@gmail.com>
2026-03-07 12:54:58 +0600

    complete sign in functioality

---
+++ script/signin.js
@@ -0,0 +1,17 @@
+const naemEl = document.getElementById("name");
+const passwordEl = document.getElementById("password");
+const submitEl = document.getElementById("submit");
+
+submitEl.addEventListener("click", (e) => {
+  e.preventDefault();
+  const name = naemEl.value;
+  const password = passwordEl.value;
+  if (name !== "admin" && password !== "admin123") {
+    alert("Invalid UserName or Password!");
+    return;
+  }
+  alert("Sign in successful!");
+  naemEl.value = "";
+  passwordEl.value = "";
+  window.location.replace("dashboard.html");
+});
