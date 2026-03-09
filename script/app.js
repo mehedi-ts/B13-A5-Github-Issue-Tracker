de12beba8318d83946a55accc558aa33f96b657b
mehedi hasan <mehedihasan.j135@gmail.com>
2026-03-09 19:33:43 +0600

    update features

--- script/app.js
+++ script/app.js
@@ -1,19 +1,23 @@
 const allbtn = document.getElementById("all-btn");
 const openbtn = document.getElementById("open-btn");
 const closedbtn = document.getElementById("closed-btn");
+const search = document.getElementById("search");
 
 allbtn.addEventListener("click", () => {
   openbtn.classList.remove("btn-primary");
   closedbtn.classList.remove("btn-primary");
   allbtn.classList.add("btn-primary");
+  filterByStatus("all");
 });
 openbtn.addEventListener("click", () => {
   closedbtn.classList.remove("btn-primary");
   allbtn.classList.remove("btn-primary");
   openbtn.classList.add("btn-primary");
+  filterByStatus("open");
 });
 closedbtn.addEventListener("click", () => {
   allbtn.classList.remove("btn-primary");
   openbtn.classList.remove("btn-primary");
   closedbtn.classList.add("btn-primary");
+  filterByStatus("closed");
 });
--- script/machine.js
+++ script/machine.js
@@ -1,5 +1,57 @@
 let allData = [];
 
+const filterByStatus = (sta) => {
+  if (sta === "all") {
+    displayData(allData);
+    return;
+  }
+  const filterData = allData.filter((data) => data.status === sta);
+  displayData(filterData);
+};
+const srcIssues = () => {
+  const srcText = document.getElementById("search").value;
+  console.log(srcText);
+  let url = `https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${srcText}`;
+  console.log(url);
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      allData = data.data;
+      displayData(data.data);
+      console.log(data.data);
+    });
+};
+
+const cardMOdal = (issue) => {
+  const date = issue.createdAt.split("T")[0];
+  const title = document.getElementById("title");
+  const status = document.getElementById("status");
+  const opendBy = document.getElementById("opendby");
+  const dateEl = document.getElementById("date");
+  const pr = document.getElementById("pr");
+  const assigne = document.getElementById("assigne");
+  const prio = document.getElementById("prio");
+  const labels = document.getElementById("labels");
+
+  title.innerText = issue.title;
+  status.innerText = issue.status;
+  opendBy.innerText = issue.author;
+  dateEl.innerText = date;
+  pr.innerText = issue.description;
+  assigne.innerText = issue.assignee;
+  prio.innerText = issue.priority;
+  labels.innerHTML = "";
+  issue.labels.forEach((element) => {
+    const span = document.createElement("span");
+    span.classList =
+      "rounded-full px-4 py-2 text-xs font-medium bg-[#FFF8DB] text-[#D97706]";
+    span.innerText = element;
+    labels.appendChild(span);
+  });
+
+  document.getElementById("my_modal_6").checked = true;
+};
+
 const dataLoad = (url) => {
   fetch(url)
     .then((res) => res.json())
@@ -13,18 +65,23 @@ dataLoad("https://phi-lab-server.vercel.app/api/v1/lab/issues");
 
 const displayData = (issues) => {
   const issueCount = issues.length;
+  const countUp = document.getElementById("count");
+  countUp.innerText = issueCount;
   const issueCon = document.getElementById("issues-con");
   issueCon.innerHTML = "";
   issues.forEach((issue) => {
     const date = issue.createdAt.split("T")[0];
 
     const div = document.createElement("div");
-    div.innerHTML = `<div class="box rounded-[4px] shadow-md border-t-3  bg-white">
+    div.addEventListener("click", () => {
+      cardMOdal(issue);
+    });
+    div.innerHTML = `<div class="box h-[300px] rounded-[4px] shadow-md border-t-3 ${issue.status === "open" ? "border-[#00A96E]" : "border-[#A855F7]"}  bg-white">
           <div class="py-4 px-4 flex flex-col gap-3">
             <div class="flex items-center justify-between">
               <img class="w-6 h-6" src=${issue.status === "open" ? "../assets/Open-Status.png" : "../assets/Close-Status.png"} alt="" />
               <div
-                class="prio w-20 h-6 flex items-center justify-center bg-red-200 text-red-500 rounded-full"
+                class="prio w-20 h-6 flex items-center justify-center ${issue.priority === "high" ? "bg-[#FEECEC] text-[#EF4444]" : issue.priority === "medium" ? "bg-[#FFF6D1] text-[#F59E0B]" : "bg-[#EEEFF2] text-[#9CA3AF]"} rounded-full"
               >
                 <p class="text-[12px] font-medium">${issue.priority}</p>
               </div>
@@ -36,13 +93,11 @@ const displayData = (issues) => {
               <p class="text-[12px] text-[#64748B]">
                 ${issue.description}
               </p>
-              <div class="tags flex gap-2 mt-3">
+              <div class="tags flex flex-wrap gap-2 mt-3">
               ${issue.labels
                 .map((lable) => {
-                  return `<span
-                  class=" flex items-center justify-center border rounded-full  "
-                  ><p>${lable}</p></span
-                >
+                  return `<p class="rounded-full flex gap-1 px-4 py-1 text-xs font-medium ${lable === "help wanted" ? "bg-[#FFF8DB] text-[#D97706]" : lable === "bug" ? "bg-[#FEECEC] text-[#EF4444]" : "bg-[#DEFCE8] text-[#00A96E]"}"><img class=" w-[12px]" src=${lable === "bug" ? "./assets/bug.svg" : lable === "help wanted" ? "./assets/lifebuoy.svg" : "./assets/enh.svg"} alt="" /> ${lable}</p>
+                
                 `;
                 })
                 .join("")}
